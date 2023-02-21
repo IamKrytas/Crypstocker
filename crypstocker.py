4dd3a3d82c6736539fe90c7f6969eec85e81d991
IamKrytas <96568740+IamKrytas@users.noreply.github.com>
2023-02-21 20:32:05 +0100

    Update crypstocker.py

--- crypstocker.py
+++ crypstocker.py
@@ -1,24 +1,30 @@
-#Temp_name         Crypstocker
+#name              Crypstocker
 #author            IamKrytas
-#version           0.0.1
+#language          Python3
+#version           0.1.0
+#update            12.09.2022
+#changelog         Dodanie możliwości wprowadzania przez użytkownika kryptowaluty do pobrania
 #description       Program pobierajacy kursy kryptowalut z zapiem do pliku wraz z data i godzina
 
 import os
 import time
 import datetime
 from pycoingecko import CoinGeckoAPI
+
 os.system("cls")
-plik=open("log.txt","a")
+plik = open("log.txt","a")
+Kwaluta = input("Podaj pelna nazwe kryptowaluty: ")
+Rwaluta = input("Podaj skrot oznaczenie waluty swiatowej: ")
+ilosc = int(input("Podaj ilosc pobran: "))
+sekundy = int(input("Podaj ilosc sekund pomiedzy pobraniami: "))
 
-ilosc = int(input("Podaj ilosc: "))
-sekundy = int(input("Podaj ilosc sekund: "))
-
-krok=0
-while krok<ilosc:
+krok = 1
+while krok<=ilosc:
     czas=datetime.datetime.now()
     plik.write(str(czas) + "\n")
     cg=CoinGeckoAPI()
-    x=cg.get_price(ids=['bitcoin', 'ethereum', 'ripple',], vs_currencies='pln')
+    x=cg.get_price(ids=Kwaluta, vs_currencies=Rwaluta)
+    print(x)
     plik.write(str(x) + "\n"+ "\n")
     time.sleep(sekundy)
     krok=krok+1
